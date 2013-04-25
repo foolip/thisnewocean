e9686ed2dd0891e54c55e14ea5293392e9abe4e1
Philip Jägenstedt <philip@foolip.org>
2013-04-25 20:50:07 +0700

    Make more project-specific changes to sanitize

--- sanitize.py
+++ sanitize.py
@@ -123,6 +123,32 @@ def externalizeWhitespace(tagNames):
             elm.lastChild.data = elm.lastChild.data.rstrip()
         pad(elm, '\n')
 
+# group images and their captions into figures
+def figurize(fig):
+    def nextElement(ref):
+        n = ref.nextSibling
+        while n and n.nodeType != n.ELEMENT_NODE:
+            assert isEmpty(n)
+            n = n.nextSibling
+        return n
+    imgs = list(iterTags(fig, 'img'))
+    if len(imgs) > 0:
+        caption = nextElement(fig)
+        if caption.tagName == 'p':
+            hr = nextElement(caption)
+        else:
+            hr = caption
+            caption = None
+        if hr.tagName == 'hr':
+            fig.tagName = 'div'
+            fig.setAttribute('class', 'figure')
+            if caption != None:
+                caption.setAttribute('class', 'caption')
+                fig.appendChild(caption)
+            remove(hr)
+            return True
+    return False
+
 # return the first element matching tagName
 def first(tagName):
     return next(iterTags(doc, tagName), None)
@@ -356,9 +382,22 @@ def quotify(elm, exclude=None):
 body = first('body')
 
 removeAttributes({'body': ['bgcolor'],
-                  'img': ['align', 'height', 'hspace', 'vspace', 'width']})
+                  'img': ['align', 'height', 'hspace', 'vspace', 'width'],
+                  'table': ['align', 'border']})
 
 removeComments()
+
+mapTags({'cite': 'i', 'em': 'i', 'strong': 'b'})
+
+removeEmpty(['b', 'i', 'li', 'p', 'sub', 'sup'])
+
+removeMeta()
+
+# figurize
+for p in iterTags(doc, 'p'):
+    if p.hasAttribute('align') and p.getAttribute('align') == 'center':
+        if figurize(p):
+            p.removeAttribute('align')
 
 # remove the footer (empty elements and images at end of body)
 for n in reversed(list(iterNodes(body))):
@@ -370,10 +409,6 @@ for n in reversed(list(iterNodes(body))):
         remove(n)
     else:
         break
-
-removeEmpty(['b', 'i', 'li', 'p', 'sub', 'sup'])
-
-mapTags({'em': 'i', 'cite': 'i', 'strong': 'b'})
 
 paragraphize(body)
 for bq in iterTags(doc, 'blockquote'):
